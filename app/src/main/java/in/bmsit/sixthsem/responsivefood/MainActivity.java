64ea49780d02e5e0a8f5d86ed3d841b71da26493
Santhosh1204 <santhoshkumara1204@gmail.com>
2021-06-27 11:36:09 +0530

    added  SignUp_activity

--- app/src/main/java/in/bmsit/sixthsem/responsivefood/MainActivity.java
+++ app/src/main/java/in/bmsit/sixthsem/responsivefood/MainActivity.java
@@ -2,13 +2,44 @@ package in.bmsit.sixthsem.responsivefood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button login,signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
+        getSupportActionBar().hide();
+        login=findViewById(R.id.btnlogin);
+        signup=findViewById(R.id.btnsignUp);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Login has been clicked", Toast.LENGTH_LONG).show();
+                Intent intent=new Intent(MainActivity.this,SignIn_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent=new Intent(MainActivity.this,SignUp_activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/in/bmsit/sixthsem/responsivefood/SignUp_activity.java
@@ -0,0 +1,15 @@
+package in.bmsit.sixthsem.responsivefood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUp_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        getSupportActionBar().setTitle("Sign Up");
+    }
+}
